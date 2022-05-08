af2ed48793311f4eb14af063f6fb9c17b52f66ba
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2022-05-08 22:32:51 +0300

    Initial commit - v1.0.7

---
+++ README.md
@@ -0,0 +1,16 @@
+# Quran Chatbot
+
+This is a quran chatbot that works through telegram messenger.
+
+# Try it out
+
+Use the following links to talk to the bot live:
+
+## Telegram Messenger
+
+Talk to the bot through Telegram messenger. Scan the QR code or use the Telegram link below.
+
+<p>   
+<img src="https://raw.githubusercontent.com/hashirabdulbasheer/my_assets/master/quran_chatbot_qr_code.png" width="100">
+
+[https://t.me/quran_chat_bot](https://t.me/quran_chat_bot)
---
+++ flask_app.py
@@ -0,0 +1,185 @@
+
+from flask import Flask, request, json
+import telegram
+import os
+import random
+
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+
+TOKEN = ''
+bot = telegram.Bot(TOKEN)
+
+
+HELP_MESSAGE = """Assalamu Alaikum, Welcome to the Noble Quran chatbot.
+
+    To get any ayat and its english translation, send a message in surah:ayat format.
+    For eg. send message '2:255' to get ayatul kursi in both english and arabic.
+
+    To get the arabic ayat alone send message in ar surah:ayat format.
+    For eg. send message 'ar 2:255' to get arabic ayatul kursi.
+
+    To get the english ayat alone, send message in en surah:ayat format.
+    For eg. send message 'en 2:255' to get english translation of ayatul kursi.
+
+    Commands:
+        /start - Help on how to use the bot
+        /index - List of surahs with indices
+        /random - A random ayat
+        /version - Version of the bot
+        /feedback - Send feedback
+"""
+VERSION = "1.0.7"
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def quran_bot_webhook():
+    update = telegram.update.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    text = update.message.text.encode('utf-8').decode()
+    text = text.lower()
+    # the first time you chat with the bot AKA the welcoming message
+    if text == "/start":
+        bot.sendMessage(chat_id=chat_id, text=HELP_MESSAGE, reply_to_message_id=msg_id)
+    elif text == "/index":
+        index_message = get_surah_list()
+        bot.sendMessage(chat_id=chat_id, text=index_message, reply_to_message_id=msg_id)
+    elif text == "/version":
+        bot.sendMessage(chat_id=chat_id, text=VERSION, reply_to_message_id=msg_id)
+    elif text == "/random":
+        indices = get_random_indices()
+        surah = indices[0]
+        ayat = indices[1]
+        surah_name = indices[2]
+        surah_display = surah + 1
+        ayat_display = ayat + 1
+        index_str = "Surah {} ({}:{}) ".format(surah_name, surah_display, ayat_display)
+        ar_ayat = "{}\n{}".format(index_str, get_ayat_ar(surah, ayat))
+        en_ayat = "{}\n{}".format(index_str, get_ayat_en(surah, ayat))
+        bot.sendMessage(chat_id=chat_id, text=ar_ayat)
+        bot.sendMessage(chat_id=chat_id, text=en_ayat)
+    elif text == "/feedback":
+        feedback_msg = "For feedback and suggestions, please send an email to support@uxquran.com"
+        bot.sendMessage(chat_id=chat_id, text=feedback_msg, reply_to_message_id=msg_id)
+    else:
+       try:
+            is_arabic = False
+            is_english = False
+            if ":" in text:
+                params = text.split(":")
+                if len(params) == 3:
+                    # word by word meaning
+                    surah = int(params[0]) - 1
+                    ayat  = int(params[1]) - 1
+                    word  = int(params[2]) - 1
+
+                    if surah > 114 or surah == 114:
+                        bot.sendMessage(chat_id=chat_id, text="Sorry, That's an invalid surah number!", reply_to_message_id=msg_id)
+                    else:
+                        surah_name = get_surah_title(surah)
+                        surah_display = surah + 1
+                        ayat_display = ayat + 1
+                        word_display = word + 1
+                        index_str = "Surah {} ({}:{}:{}) ".format(surah_name, surah_display, ayat_display, word_display)
+                        en_ayat = "{}\n{}".format(index_str, get_ayat_word(surah, ayat, word))
+                        bot.sendMessage(chat_id=chat_id, text=en_ayat)
+                else:
+                    # normal sentences
+                    if "ar" in text:
+                        is_arabic = True
+                        text = text.replace("ar", "")
+                        text = text.strip()
+                    elif "en" in text:
+                        is_english = True
+                        text = text.replace("en", "")
+                        text = text.strip()
+
+                    surah = int(params[0]) - 1
+                    ayat  = int(params[1]) - 1
+
+                    if surah > 114 or surah == 114:
+                        bot.sendMessage(chat_id=chat_id, text="Sorry, That's an invalid surah number!", reply_to_message_id=msg_id)
+                    else:
+                        surah_name = get_surah_title(surah)
+                        surah_display = surah + 1
+                        ayat_display = ayat + 1
+                        index_str = "Surah {} ({}:{}) ".format(surah_name, surah_display, ayat_display)
+
+                        ar_ayat = "{}\n{}".format(index_str, get_ayat_ar(surah, ayat))
+                        en_ayat = "{}\n{}".format(index_str, get_ayat_en(surah, ayat))
+                        # reply_message = "{}\n\n{}".format(quran_ar, quran_en)
+                        if is_arabic:
+                            bot.sendMessage(chat_id=chat_id, text=ar_ayat)
+                        elif is_english:
+                            bot.sendMessage(chat_id=chat_id, text=en_ayat)
+                        else:
+                            bot.sendMessage(chat_id=chat_id, text=ar_ayat)
+                            bot.sendMessage(chat_id=chat_id, text=en_ayat)
+            else:
+                bot.sendMessage(chat_id=chat_id, text=HELP_MESSAGE, reply_to_message_id=msg_id)
+
+       except Exception as e:
+           # if things went wrong
+          #bot.sendMessage(chat_id=chat_id, text=str(e), reply_to_message_id=msg_id)
+          error_message = "Sorry, I am not able to understand that. For help send /start."
+          bot.sendMessage(chat_id=chat_id, text=error_message, reply_to_message_id=msg_id)
+
+    return 'ok'
+
+def get_ayat_ar(surah, ayat):
+    json_data = open(os.path.join(app.static_folder, "quran-ar.json"), "r")
+    data = json.load(json_data)
+    if surah > 114 or surah == 114:
+        return "Invalid surah number. Please send a surah number between 1 and 114."
+    if ayat > len(data["quran"]["sura"][surah]["aya"]) or ayat == len(data["quran"]["sura"][surah]["aya"]):
+        surahIndex = surah + 1
+        return "Invalid ayat number. Surah {} has only {} ayats".format(surahIndex, len(data["quran"]["sura"][surah]["aya"]))
+    else:
+        return data["quran"]["sura"][surah]["aya"][ayat]["text"]
+
+def get_ayat_en(surah, ayat):
+    json_data = open(os.path.join(app.static_folder, "sahih.json"), "r")
+    data = json.load(json_data)
+    if surah > 114 or surah == 114:
+        return "Invalid surah number. Please send a surah number between 1 and 114."
+    if ayat > len(data["quran"]["sura"][surah]["aya"]) or ayat == len(data["quran"]["sura"][surah]["aya"]):
+        surahIndex = surah + 1
+        return "Invalid ayat number. Surah {} has only {} ayats".format(surahIndex, len(data["quran"]["sura"][surah]["aya"]))
+    else:
+        return data["quran"]["sura"][surah]["aya"][ayat]["text"]
+
+def get_ayat_word(surah, ayat, word):
+    json_data = open(os.path.join(app.static_folder, "word", "{}.json".format(surah)))
+    data = json.load(json_data)
+    if surah > 114 or surah == 114:
+        return "Invalid surah number. Please send a surah number between 1 and 114."
+    if ayat > len(data) or ayat == len(data):
+        surahIndex = surah + 1
+        return "Invalid ayat number. Surah {} has only {} ayats".format(surahIndex, len(data))
+    else:
+        return "{}\n{}".format(data[ayat][word]["ar"], data[ayat][word]["tr"])
+
+def get_surah_list():
+    json_data = open(os.path.join(app.static_folder, "surahlist.json"), "r")
+    data = json.load(json_data)
+    index_message = "**Surah List**\n\n"
+    for surah in data:
+        index_message = index_message + "{}. {}\n".format(surah["number"], surah["transliteration_en"])
+    return index_message
+
+def get_random_indices():
+    json_data = open(os.path.join(app.static_folder, "surahlist.json"), "r")
+    data = json.load(json_data)
+    surah = random.randint(0, 113)
+    ayat = random.randint(0, data[surah]["total_verses"]-1)
+    return surah, ayat, data[surah]["transliteration_en"]
+
+def get_surah_title(surah):
+    json_data = open(os.path.join(app.static_folder, "surahlist.json"), "r")
+    data = json.load(json_data)
+    return data[surah]["transliteration_en"]
+
